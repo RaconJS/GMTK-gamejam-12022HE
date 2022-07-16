95509b6f1afdacb92ca5847d394593ef1a094ae9
bigdog0044 <jambo.peach@outlook.com>
2022-07-16 11:15:05 +0100

    updating files

---
+++ bigdog0044/Weapon handling/Movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 3f;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float xMove = Input.GetAxisRaw("Horizontal"); // d key changes value to 1, a key changes value to -1
+        float zMove = Input.GetAxisRaw("Vertical"); // w key changes value to 1, s key changes value to -1
+        rb.velocity = new Vector3(xMove, rb.velocity.y, zMove) * speed; // Creates velocity in direction of value equal to keypress (WASD). rb.velocity.y deals with falling + jumping by setting velocity to y. 
+
+    }
+} 
