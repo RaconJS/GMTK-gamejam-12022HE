94cb3f8ef675398980fcbb8db36c084d2406cc10
bigdog0044 <jambo.peach@outlook.com>
2022-07-16 13:35:19 +0100

    updated scripts

--- bigdog0044/Weapon handling/Movement.cs
+++ bigdog0044/Weapon handling/Movement.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed = 3f;
+    private GameObject player;
+    private Vector3 player_position;
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-
+        player = GameObject.Find("player");
+        
         rb = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
@@ -19,5 +22,8 @@ public class Movement : MonoBehaviour
         float zMove = Input.GetAxisRaw("Vertical"); // w key changes value to 1, s key changes value to -1
         transform.Translate(Vector3.forward * Time.deltaTime * speed * zMove);
         transform.Translate(Vector3.right * Time.deltaTime * speed * xMove);
+
+        player_position = player.transform.position;
+        Debug.Log(player_position);
     }
 } 
