766cadcab83f05bc48f4a875cf13016db9e5b21f
qwerty9118 <williamdbmayberry@gmail.com>
2022-07-16 11:12:24 +0100

    updating movement, adding key options

--- qwerty9118/movePlayer.cs
+++ qwerty9118/movePlayer.cs
@@ -7,6 +7,13 @@ public class movePlayer : MonoBehaviour
 {
     public int nrg;
     public bool isMoving;
+    public KeyCode keyReload = KeyCode.R;
+    public KeyCode keyLaunch = KeyCode.E;
+    public KeyCode keyStartMove = KeyCode.Q;
+    public KeyCode keyUp = KeyCode.UpArrow;
+    public KeyCode keyDown = KeyCode.DownArrow;
+    public KeyCode keyLeft = KeyCode.LeftArrow;
+    public KeyCode keyRight = KeyCode.RightArrow;
     private Vector2 direction;
     private Rigidbody2D rb;
 
@@ -16,77 +23,68 @@ public class movePlayer : MonoBehaviour
         nrg = 10;
         isMoving = false;
         rb = GetComponent<Rigidbody2D>();
-        StartMovement();
     }
 
     void StartMovement(){
         isMoving = true;
-        direction = new Vector2();
+        direction = new Vector2(0, 0);
         GetComponent<Renderer>().material.SetColor("_Color", Color.red);
     }
 
     private float xMovement(float xDirection)
     {
-        float posOrNeg = xDirection / Mathf.Abs(xDirection);
-        switch (Mathf.Abs(xDirection))
+        if (xDirection != 0)
         {
-            case 0:
-                return 1 * posOrNeg;
-            case 1:
-                return 1.25f * posOrNeg;
-            case 2:
-                return 1.5f * posOrNeg;
-            case 3:
-                return 1.75f * posOrNeg;
-            case 4:
-                return 2 * posOrNeg;
-            default:
-                return xDirection;
+            return ((2 * Mathf.Abs(xDirection) + 4) / 3) * (xDirection / Mathf.Abs(xDirection));
         }
-
+        return 0;
     }
 
     private float yMovement(float yDirection)
     {
-        return yDirection * 2;
+        if (yDirection != 0)
+        {
+            return (2 * yDirection + 4) / 3;
+        }
+        return 0;
     }
 
     void Update(){
         if(isMoving)
         {
-             if(Input.GetKeyDown(KeyCode.LeftArrow)){
+             if(Input.GetKeyDown(keyLeft)){
                 direction.x -= 1;
                 Debug.Log(direction.x+", "+direction.y);
             }
-            if (Input.GetKeyDown(KeyCode.RightArrow))
+            if (Input.GetKeyDown(keyRight))
             {
                 direction.x += 1;
                 Debug.Log(direction.x + ", " + direction.y);
             }
-            if (Input.GetKeyDown(KeyCode.UpArrow))
+            if (Input.GetKeyDown(keyUp))
             {
                 direction.y += 1;
                 Debug.Log(direction.x + ", " + direction.y);
             }
-            if (Input.GetKeyDown(KeyCode.DownArrow))
+            if (Input.GetKeyDown(keyDown))
             {
                 direction.y -= 1;
                 Debug.Log(direction.x + ", " + direction.y);
             }
-            if (Input.GetKeyDown(KeyCode.E))
+            if (Input.GetKeyDown(keyLaunch))
             {
                 isMoving = false;
-                rb.velocity = new Vector2(xMovement(direction.x), yMovement(direction.y)) * 2;
+                rb.velocity = new Vector2(xMovement(direction.x), yMovement(direction.y));
                 GetComponent<Renderer>().material.SetColor("_Color", Color.white);
-                Debug.Log(direction.x + ", " + direction.y);
+                Debug.Log(rb.velocity.x + ", " + rb.velocity.y);
             }
         }
-        else if (Input.GetKeyDown(KeyCode.Q))
+        else if (Input.GetKeyDown(keyStartMove))
         {
             StartMovement();
         }
 
-        if(Input.GetKeyDown(KeyCode.R))
+        if(Input.GetKeyDown(keyReload))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
