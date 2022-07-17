ece8bd98debc936a07867246e7d92bd3cd26a3ef
qwerty9118 <williamdbmayberry@gmail.com>
2022-07-17 09:20:06 +0100

    Attempt at getting dice value

--- qwerty9118/dieMovement.cs
+++ qwerty9118/dieMovement.cs
@@ -1,35 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class dieMovement : MonoBehaviour
 {
     private Vector3 gravity;
-    private Rigidbody rb;
+    public bool rolling;
+    public bool startRoll;
+    public GameObject die;
+    public int result;
+
+    public int rollDice()
+    {
+        int[] result = rollDie().ToArray();
+        Debug.Log(result.Length);
+        Debug.Log(result[0]);
+        return result[0];
+    }
+
+    private IEnumerable<int> rollDie()
+    {
+        GameObject dieInstance = Instantiate(die, new Vector3(0, 0, -15), Random.rotation);
+        while (dieInstance.GetComponent<dieMovement>().rolling)
+        {
+            Thread.Sleep(3000);
+        }
+        yield return dieInstance.GetComponent<dieMovement>().result;
+    }
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        gravity = new Vector3(0, 0, 9.85f);
-        rb = GetComponent<Rigidbody>();
+        rolling = true;
     }
 
     void Update()
     {
-        if (rb.velocity.magnitude < 0.1)
+        if(startRoll == false) {
+            rolling = true;
+            startRoll = true;
+        }
+        if (GetComponent<Rigidbody>().velocity.magnitude < 0.1 && rolling)
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(transform.position);
-            if (Physics.Raycast(ray, out hit))
+            int closestIndex = 0;
+            float minZ = 100;
+
+            for (int i = 0; i < transform.childCount; i++)
             {
-                Debug.Log(hit.triangleIndex);
+                if (transform.GetChild(i).position.z < minZ)
+                {
+                    minZ = transform.GetChild(i).position.z;
+                    closestIndex = i;
+                }
             }
+
+            Debug.Log(transform.GetChild(closestIndex).name);
+
+            rolling = false;
+
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rollDice();
         }
     }
 
     private void FixedUpdate()
     {
-        rb.velocity += gravity/50;
+        GetComponent<Rigidbody>().velocity += new Vector3(0, 0, 9.85f) / 50;
     }
 }
