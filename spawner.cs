1cad98b038efb6c64ec6020e91b17b6479f7dbbd
racon <46864506+RaconJS@users.noreply.github.com>
2022-07-17 18:03:48 +0100

    asd

--- spawner.cs
+++ spawner.cs
@@ -7,7 +7,7 @@ public class spawner : MonoBehaviour
 {
 	public Tilemap tileMap;
 	public GameObject spawnObj;
-	public float probability=0.02f;
+	public float probability=0.1f;
 	public float probScale=0.1f;
 	// Start is called before the first frame update
 	void Start()
@@ -16,10 +16,10 @@ public class spawner : MonoBehaviour
 		float tilesLeft;
 		for(int y=0;y<len[1];++y){
 			for(int x=0;x<len[0];++x){
-				if(tileMap.HasTile(new Vector3((float)x,(float)y,0))){
+				if(tileMap.HasTile(new Vector3Int(x,y,0))){
 					float val=Random.Range(0f,1f);
-					if(val**(1+y*probScale)<probability){
-						Instantiate(spawnObj,Vector2((float)x+0.5f,(float)y+1f));
+					if(Mathf.Pow(val,(1+y*probScale))<probability){
+						Instantiate(spawnObj,new Vector3((float)x+0.5f,(float)y+1f),transform.rotation);
 					}
 				}
 			}
