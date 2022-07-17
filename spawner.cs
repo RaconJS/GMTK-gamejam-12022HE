8beef0235519ecbc26a14f818d9227bc7e0e6d56
racon <46864506+RaconJS@users.noreply.github.com>
2022-07-17 17:34:37 +0100

    spawner2

--- spawner.cs
+++ spawner.cs
@@ -6,14 +6,19 @@ using UnityEngine.Tilemaps;
 public class spawner : MonoBehaviour
 {
 	public Tilemap tileMap;
-	public GameObject enemyPrefab;
+	public GameObject spawnObj;
+	public float 
 	// Start is called before the first frame update
 	void Start()
 	{
 		int[] len={tileMap.size.x,tileMap.size.y};
+		float tilesLeft;
 		for(int y=0;y<len[1];++y){
 			for(int x=0;x<len[0];++x){
-
+				if(tileMap.HasTile(new Vector3(x,y,0))){
+					float val=Random.Range(0f,1f);
+					if(val>0)
+				}
 			}
 		}
 	}
